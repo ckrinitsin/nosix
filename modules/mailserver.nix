7d15aaf99ea96be0dec59de83db00493125af701
Christian Krinitsin <code@krinitsin.xyz>
2025-02-26 13:01:19 +0100

    add mailserver

---
+++ modules/mailserver.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }: {
+  imports = [
+    (builtins.fetchTarball {
+      url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/nixos-24.11/nixos-mailserver-nixos-24.11.tar.gz";
+      sha256 = "05k4nj2cqz1c5zgqa0c6b8sp3807ps385qca74fgs6cdc415y3qw";
+    })
+  ];
+
+  mailserver = {
+    enable = true;
+    fqdn = "mail.krinitsin.com";
+    domains = [ "krinitsin.com" ];
+    certificateScheme = "acme-nginx";
+
+    # To create the password hashes, use nix-shell -p mkpasswd --run 'mkpasswd -sm bcrypt'
+    loginAccounts = {
+      "mail@krinitsin.com" = {
+        hashedPasswordFile = "/secret/mail@krinitsin.com";
+        aliases = ["postmaster@krinitsin.com"];
+      };
+      "wladislaw@krinitsin.com" = {
+        hashedPasswordFile = "/secret/wladislaw@krinitsin.com";
+      };
+    };
+  };
+
+  services.roundcube = {
+     enable = true;
+     hostName = "webmail.krinitsin.com";
+     extraConfig = ''
+       $config['smtp_host'] = "tls://${config.mailserver.fqdn}";
+       $config['smtp_user'] = "%u";
+       $config['smtp_pass'] = "%p";
+     '';
+  };
+}
