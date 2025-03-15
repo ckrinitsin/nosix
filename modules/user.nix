cda8563ae6fa79a531347c84f7e380d95402c133
Christian Krinitsin <code@krinitsin.xyz>
2025-03-15 12:52:36 +0100

    change public ssh-key

--- modules/user.nix
+++ modules/user.nix
@@ -4,7 +4,10 @@
   users.users.admin = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZxiAIsF13XqqxG0QzGFhT3iLDMsu2snb0wJOPUUq8e chris@deskpin" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDwlqdl/70dJ1ABKwLEdLB6/RDpfE4RVaB+xL3YJ1v3+ chris@kingpin" ];
+    openssh.authorizedKeys.keys = [ 
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBi+VYFQOfb7J2eJd8CiXKcaLD+QfztXiS2pU07oHZNr chris@archiso"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDwlqdl/70dJ1ABKwLEdLB6/RDpfE4RVaB+xL3YJ1v3+ chris@kingpin"
+    ];
   };
   security.sudo.wheelNeedsPassword = false;
 
