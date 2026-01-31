c911efa5bf9b0c89d620b53a4cd0bb00c0de7ef2
Christian Krinitsin <mail@krinitsin.com>
2026-01-31 22:14:00 +0100

    Add colmena

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    colmena.url = "github:zhaofengli/colmena";
   };
 
-outputs = { self, nixpkgs, nixpkgs-unstable }: {
+outputs = { self, nixpkgs, nixpkgs-unstable, colmena }: {
     nixosConfigurations = {
       nosix = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -22,5 +23,32 @@ outputs = { self, nixpkgs, nixpkgs-unstable }: {
         ];
       };
     };
+
+ colmena = {
+      meta = {
+        nixpkgs = import nixpkgs { system = "x86_64-linux"; };
+        specialArgs = {
+          unstable = import nixpkgs-unstable {
+            system = "x86_64-linux";
+            config.allowUnfree = true;
+          };
+        };
+      };
+
+      # Define your server
+      nosix = { name, nodes, ... }: {
+        deployment = {
+          targetHost = "krinitsin.com";  # or IP address
+          targetUser = "root";  # or another user with sudo
+        };
+
+        # Import your existing configuration
+        imports = [
+          ./configuration.nix
+	  ./hardware-configuration.nix
+        ];
+      };
+    };
+
   };
 }
